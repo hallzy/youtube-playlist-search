311aa975db1fd5815e1456ac1eb306676bc6ec6f
Steven Hall <Hallzy.18@gmail.com>
2022-02-12 14:28:02 -0800

    Comments for background script

--- js/background.js
+++ js/background.js
@@ -1,14 +1,25 @@
+// Runs when this extension is installed or updated, or if Chrome is updated.
 chrome.runtime.onInstalled.addListener(function() {
+    // Remove all rules
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-        chrome.declarativeContent.onPageChanged.addRules([
-            {
-                conditions: [
-                    new chrome.declarativeContent.PageStateMatcher({
-                        pageUrl: { urlMatches: 'youtube.com.*list=' },
-                    })
-                ],
-                actions: [ new chrome.declarativeContent.ShowPageAction() ]
-            }
-        ]);
+        // Add new rules
+        chrome.declarativeContent.onPageChanged.addRules(
+            // Array of rules to add
+            [
+                {
+                    // Conditions for this rule that triggers the actions
+                    // We only have one condition which is that the URL must be
+                    // a playlist URL
+                    conditions: [
+                        new chrome.declarativeContent.PageStateMatcher({
+                            pageUrl: { urlMatches: 'youtube.com.*list=' },
+                        })
+                    ],
+                    // If the above condition is met, we do this.
+                    // This tells activates the extension to be actually used
+                    actions: [ new chrome.declarativeContent.ShowAction() ]
+                }
+            ]
+        );
     });
 });
